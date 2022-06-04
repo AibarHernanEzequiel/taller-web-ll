b2f4d1db89c285a84f0e239b00a86aa9728d18f6
Galeano <carla.nahir.galeano@accenture.com>
2022-06-04 01:44:00 -0300

    ProyectoBootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { TiendaComponent } from './tienda/tienda.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    TiendaComponent
   ],
   imports: [
     BrowserModule,
