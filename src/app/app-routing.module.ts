e998df83fb9ecf3f035e19e826aa1356e150c62b
IanWagnerLautaro <ianwagnerlautaro@gmail.com>
2022-06-20 19:37:37 -0300

    b

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { ModuleWithProviders } from '@angular/core';
 import { BigTiendaComponent } from './components/big-tienda/big-tienda.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProductoDetalleComponentComponent } from './producto-detalle-component/producto-detalle-component.component';
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path: 'home', component: HomeComponent},
   { path: 'login', component: LoginComponent },
-  { path: 'big-tienda', component: BigTiendaComponent}
+  { path: 'big-tienda', component: BigTiendaComponent},
+  {path:'detalle/:id', component:ProductoDetalleComponentComponent},
 
 ];
 
