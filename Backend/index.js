9c075d6ba5599922fefc7572163fdadea2d68040
carlanahirgaleano <carla.nahir.galeano@accenture.com>
2022-06-22 23:07:25 -0300

    Se agrega el archivo de backend

---
+++ Backend/index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var app = require('./app');
+var port = 3900;
+
+//Para desactivar metodos que estan deprecados
+//mongoose.set('useFindAndModify', false);
+
+//Para trabajar con promesas con mongodb y manejar los errores.
+mongoose.Promise = global.Promise;
+
+//Se conecta a la base de datos uri / nombre de bdd que creamos
+
+mongoose.connect('mongodb://localhost:27017/taller-web', { useNewUrlParser : true})
+        .then(() =>{
+            console.log('La conexion a la base de datos se ha realizado correctamente!')
+            //Creamos el servidor y escuchamos peticiones http
+
+            app.listen(port, ()=>{
+                console.log("Servidor corriendo en http://localhost:" + port )
+            });
+
+});
