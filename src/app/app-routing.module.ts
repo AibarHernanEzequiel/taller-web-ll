db2c44f42a729a895698490e4b182b1f4fc5548f
Luz juarez <ljuarez@circostudio.com>
2022-06-23 16:33:32 -0300

    componentes login creados, falta service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { BigTiendaComponent } from './components/big-tienda/big-tienda.component';
+import { LoginComponent } from './components/login/login.component';
 
 import { HomeComponent } from './components/home/home.component';
 import { ProductoDetalleComponentComponent } from './producto-detalle-component/producto-detalle-component.component';
@@ -10,11 +11,13 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'big-tienda', component: BigTiendaComponent },
   { path: 'detalle/:id', component: ProductoDetalleComponentComponent },
+  { path: 'login', component: LoginComponent },
   {
     path: 'usuario',
     loadChildren: () =>
       import('./modules/admin/admin.module').then((m) => m.AdminModule),
   },
+
 ];
 
 @NgModule({
